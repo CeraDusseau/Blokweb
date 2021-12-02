ac8e832b3f8fba3806be01928a9521ad297deef4
CeraDusseau <94113150+CeraDusseau@users.noreply.github.com>
2021-12-02 13:41:26 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -5,5 +5,15 @@ deButton.addEventListener("click", toggleMenu);
 
 function toggleMenu(event) {
     deNav = event.target.parentNode;
-    deNav.classList.toggle("toonMenu");
+    deNav.classList.add("toonMenu");
+}
+
+var deexitButton = document.querySelector("header nav button:nth-of-type(2)");
+
+deexitButton.addEventListener("click", exitMenu);
+
+function exitMenu(event) {
+    deNav = event.target.parentNode;
+    deNav.classList.remove("toonMenu");
+    deexitButton.classList.add("exitknop");
 }
