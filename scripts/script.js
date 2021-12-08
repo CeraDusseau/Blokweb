1412cabadd99f5d657aaf8dbb351559fa6c20436
CeraDusseau <94113150+CeraDusseau@users.noreply.github.com>
2021-12-08 14:44:16 +0100

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,23 @@
+// JavaScript Document
+var deButton = document.querySelector("nav button");
+
+deButton.addEventListener("click", toggleMenu);
+
+function toggleMenu(event) {
+    deNav = event.target.parentNode;
+    deNav.classList.add("toonMenu");
+}
+
+var deexitButton = document.querySelector("header nav button:nth-of-type(2)");
+
+deexitButton.addEventListener("click", exitMenu);
+
+function exitMenu(event) {
+    deNav = event.target.parentNode;
+    deNav.classList.remove("toonMenu");
+    deexitButton.classList.add("exitknop");
+}
+
+var afspeelButton = document.querySelector("main article div button");
+
+afspeelButton.addEventListener("click", )
